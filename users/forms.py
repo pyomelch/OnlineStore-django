d011d66e6b27cf15b98ecb517601b47197bbb984
pyomelch <omelch.py@gmail.com>
2023-08-15 13:06:18 +0300

    Added logical for Profile & edit Profile & edit image profile

--- users/forms.py
+++ users/forms.py
@@ -51,23 +51,11 @@ class UserRegistrationForm(UserCreationForm):
 
 
 class UserProfileForm(UserChangeForm):
-    first_name = forms.CharField(widget=forms.TextInput(attrs={
-        "class": "form-control py-4"
-    }))
-    last_name = forms.CharField(widget=forms.TextInput(attrs={
-        "class": "form-control py-4"
-    }))
-    image = forms.ImageField(widget=forms.FileInput(attrs={
-        "class": "custom-file-input"
-    }))
-    username = forms.CharField(widget=forms.TextInput(attrs={
-        "class": "form-control py-4",
-        "readonly": True
-    }))
-    email = forms.CharField(widget=forms.EmailInput(attrs={
-        "class": "form-control py-4",
-        "readonly": True
-    }))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control py-4"}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control py-4"}))
+    image = forms.ImageField(widget=forms.FileInput(attrs={"class": "custom-file-input"}), required=False)
+    username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control py-4", "readonly": True}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={"class": "form-control py-4", "readonly": True}))
 
     class Meta:
         model = User
--- users/views.py
+++ users/views.py
@@ -34,7 +34,15 @@ def registration(request):
 
 
 def profile(request):
-    form = UserProfileForm(instance=request.user)
+    if request.method == "POST":
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("users:profile"))
+        else:
+            print(form.errors)
+    else:
+        form = UserProfileForm(instance=request.user)
     context = {
         "title": "Store - Profile",
         "form": form
