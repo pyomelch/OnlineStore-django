3d72757f981b9e961f2071b821b75ea315182de4
pyomelch <omelch.py@gmail.com>
2023-08-15 09:22:02 +0300

    Customize User LoginForm

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,19 @@
+from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "form-control py-4",
+        "placeholder": "Enter username"
+    }))
+    password = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "form-control py-4",
+        "placeholder": "Enter password"
+    }))
+
     class Meta:
         model = User
         fields = ("username", "password")
