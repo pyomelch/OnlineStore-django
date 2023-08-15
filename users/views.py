763f643ac884c87b856e114a50d247f87e4a905f
pyomelch <omelch.py@gmail.com>
2023-08-15 13:53:47 +0300

    Refactored messages

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
@@ -26,6 +26,7 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Congratulations! You have successfully registered!")
         return HttpResponseRedirect(reverse("users:login"))
     else:
         form = UserRegistrationForm()
