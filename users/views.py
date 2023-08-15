cb3eeace7527f959ab61d988467b653730afd1bf
pyomelch <omelch.py@gmail.com>
2023-08-15 09:51:11 +0300

    Added Registration Forms % customize UserRegistrationForm

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib import auth
 from django.urls import reverse
 
 from users.models import User
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -23,4 +23,6 @@ def login(request):
 
 
 def registration(request):
-    return render(request, "users/registration.html")
+    form = UserRegistrationForm()
+    context = {"form": form}
+    return render(request, "users/registration.html", context)
