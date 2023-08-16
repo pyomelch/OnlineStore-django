eb3f33df436e9c2912012d8c5b3a4af6d2aa943d
pyomelch <omelch.py@gmail.com>
2023-08-16 16:09:02 +0300

    Added total_sum & total_quantity

--- users/views.py
+++ users/views.py
@@ -45,10 +45,20 @@ def profile(request):
             print(form.errors)
     else:
         form = UserProfileForm(instance=request.user)
+
+    baskets = Basket.objects.filter(user=request.user)
+    total_sum = 0
+    total_quantity = 0
+    for basket in baskets:
+        total_sum += basket.sum()
+        total_quantity += basket.quantity
+
     context = {
         "title": "Store - Profile",
         "form": form,
         "baskets": Basket.objects.filter(user=request.user),
+        "total_sum": total_sum,
+        "total_quantity": total_quantity,
     }
     return render(request, "users/profile.html", context)
 
