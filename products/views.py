2bab99ad2c66a0ed547fee6849133ea4f97fb6b6
pyomelch <omelch.py@gmail.com>
2023-08-16 15:47:50 +0300

    Added basket_remove

--- products/views.py
+++ products/views.py
@@ -30,3 +30,9 @@ def basket_add(request, product_id):
         basket.save()
 
     return HttpResponseRedirect(request.META["HTTP_REFERER"])
+
+
+def basket_remove(request, basket_id):
+    basket = Basket.objects.get(id=basket_id)
+    basket.delete()
+    return HttpResponseRedirect(request.META["HTTP_REFERER"])
