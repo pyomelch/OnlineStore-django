0fc129bfeebe49df659dea3d8609af16ec6438cc
pyomelch <omelch.py@gmail.com>
2023-08-17 16:34:01 +0300

    Refactor views.py with Class-Based Views. Added CreateView for User

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth, messages
-from django.urls import reverse
+from django.contrib import auth
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
-from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from users.models import User
+from users.forms import UserLoginForm, UserProfileForm, UserRegistrationForm
 from products.models import Basket
 
 
@@ -23,17 +25,11 @@ def login(request):
     return render(request, "users/login.html", context)
 
 
-def registration(request):
-    if request.method == "POST":
-        form = UserRegistrationForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Congratulations! You have successfully registered!")
-        return HttpResponseRedirect(reverse("users:login"))
-    else:
-        form = UserRegistrationForm()
-    context = {"form": form}
-    return render(request, "users/registration.html", context)
+class UserRegistrationView(CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = "users/registration.html"
+    success_url = reverse_lazy("users:login")
 
 
 @login_required
@@ -59,3 +55,5 @@ def profile(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse("index"))
+
+
