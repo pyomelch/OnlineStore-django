77fc4022f32f8b56c7700a05e81828daf95b0acd
pyomelch <omelch.py@gmail.com>
2023-08-21 13:44:10 +0300

    Added tests. ProductsListViewTestCase

--- products/tests.py
+++ products/tests.py
@@ -3,6 +3,8 @@ from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse
 
+from products.models import Product, ProductCategory
+
 
 class IndexViewTestCase(TestCase):
 
@@ -14,3 +16,13 @@ class IndexViewTestCase(TestCase):
         self.assertEqual(response.context_data["title"], "Store")
         self.assertTemplateUsed(response, "products/index.html")
 
+
+class ProductsListViewTestCase(TestCase):
+
+    def test_list(self):
+        path = reverse("products:index")
+        response = self.client.get(path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data["title"], "Store - Catalog")
+        self.assertTemplateUsed(response, "products/products.html")
