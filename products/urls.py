e07c18438f0982cb64abe43ec1d94fce57d45400
pyomelch <omelch.py@gmail.com>
2023-08-22 13:31:28 +0300

    Cached off

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
 from products.views import ProductListView, basket_add, basket_remove
-from django.views.decorators.cache import cache_page
 
 app_name = "products"
 
 urlpatterns = [
-    path("", cache_page(30)(ProductListView.as_view()), name="index"),
+    path("", ProductListView.as_view(), name="index"),
     path("category/<int:category_id>/", ProductListView.as_view(), name="category"),  # ../products/category/<category_id>
     path("page/<int:page>/", ProductListView.as_view(), name="paginator"),  # ../products/category/<category_id>
     path("baskets/add/<int:product_id>/", basket_add, name="basket_add"),  # ../products/baskets/add/<product_id>
