b50921005e5b696ba68d46f4727618cf56687345
pyomelch <omelch.py@gmail.com>
2023-08-22 15:45:21 +0300

    Added forms of checkout

--- orders/forms.py
+++ orders/forms.py
@@ -4,6 +4,23 @@ from orders.models import Order
 
 
 class OrderForm(forms.ModelForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "form-control",
+        "placeholder": "Petro"
+    }))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "form-control",
+        "placeholder": "Petrenko"
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        "class": "form-control",
+        "placeholder": "you@example.com"
+    }))
+    address = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "form-control",
+        "placeholder": "Ukraine, Kyiv, Centre str., 123"
+    }))
+
     class Meta:
         model = Order
         fields = (
